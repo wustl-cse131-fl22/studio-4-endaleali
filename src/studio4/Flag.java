e279b201088a6eddb6d616f6472d0495e18b7a66
seis <seis@DESKTOP-9T441MP.dhcp.wustl.edu>
2022-10-13 14:13:21 -0500

    MOTHERNORWAY

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -2,10 +2,26 @@ package studio4;
 
 import java.awt.Color;
 
+import javax.swing.SwingUtilities;
+
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenRadius(0.01);
+		StdDraw.setPenColor(0, 0, 0);
+		/*/
+		 * SwingUtilities.invokeLater(() -> {
+			int canvasHeight = 512;
+			int canvasWidth = 22 * canvasHeight / 16;
+			StdDraw.setCanvasSize(canvasWidth, canvasHeight);
+		 */
+		StdDraw.rectangle(0.5, 0.5, 0.3, 0.2); 
+		StdDraw.circle(0.5, 0.5, 0.10);
+		//double[] x = {0.3, 0.3, 0.3};
+		//double[] y = {0.3, 0.3, 0.3};
+		//StdDraw.polygon();
+		//StdDraw.line(0, 0, 0, 0);
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -18,7 +18,65 @@ public class InterpretDrawingFile {
 		JFileChooser chooser = new JFileChooser("resources");
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
-		Scanner in = new Scanner(f); //making Scanner with a File
+		Scanner in = new Scanner(f); // making Scanner with a File
+
+		/*
+		 * public class TestStdDraw { public static void main(String[] args) {
+		 * StdDraw.setPenRadius(0.05); StdDraw.setPenColor(StdDraw.BLUE);
+		 * StdDraw.point(0.5, 0.5); StdDraw.setPenColor(StdDraw.MAGENTA);
+		 * StdDraw.line(0.2, 0.2, 0.8, 0.2); } }
+		 */
+		String shape = in.next();
+		if (shape.equals("rectangle")) {
+			StdDraw.setPenRadius(0.05);
+			int red = in.nextInt();
+			int green = in.nextInt();
+			int blue = in.nextInt();
+			StdDraw.setPenColor(red, green, blue);
+			if(in.next().equals("true")) {
+				StdDraw.filledRectangle(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble());
+			}
+			else {
+				StdDraw.rectangle(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble());
+			}
+			
+		}
 		
+		else if (shape.equals("ellipse")){
+			StdDraw.setPenRadius(0.05);
+			int red = in.nextInt();
+			int green = in.nextInt();
+			int blue = in.nextInt();
+			StdDraw.setPenColor(red, green, blue);
+			if(in.next().equals("true")) {
+				StdDraw.filledEllipse(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble());
+			}
+			else {
+				StdDraw.ellipse(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.nextDouble());
+			}
+		}
+		else if (shape.equals("triangle")){
+			StdDraw.setPenRadius(0.01);
+			int red = in.nextInt();
+			int green = in.nextInt();
+			int blue = in.nextInt();
+			StdDraw.setPenColor(red, green, blue);
+			// double[] x = { 0.1, 0.2, 0.3, 0.2 };
+			// double[] y = { 0.2, 0.3, 0.2, 0.1 };
+			//StdDraw.filledPolygon(x, y);
+			String fill = in.next();
+			double [] x = {in.nextDouble(), in.nextDouble(), in.nextDouble()};
+			double [] y = {in.nextDouble(), in.nextDouble(), in.nextDouble()};
+			
+			if(fill.equals("true")) {
+				StdDraw.filledPolygon(x, y);
+			}
+			else {
+				StdDraw.line(x[0], y[0], x[1], y[1]);
+				StdDraw.line(x[1], y[1], x[2], y[2]);
+				StdDraw.line(x[2], y[2], x[0], y[0]);
+				
+			}
 	}
+}
 }
